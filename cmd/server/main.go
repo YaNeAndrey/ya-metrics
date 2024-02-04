8456c4251a2814361d7790a459b0b15ad8aea3a7
YaNeAndrey <andrey.bery@yandex.ru>
2024-02-04 10:41:13 -0800

    Iter1. First try

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,93 @@
 package main
 
-func main() {}
+import (
+    "net/http"
+	"strings"
+	"strconv"
+	
+)
+
+type MemStorage struct {
+	gauge map[string]float64
+	counter map[string]int64
+}
+
+func (ms *MemStorage) ChangeGaugeValue(name string, newValue float64) {
+	ms.gauge[name] = newValue
+
+}
+
+func (ms *MemStorage) ChangeCounterValue(name string, newValue int64) {
+	_, isExist := ms.counter[name] 
+	if isExist {
+		ms.counter[name] += newValue
+	} else {
+		ms.counter[name] = newValue
+	}
+}
+
+func checkDataAndUpdateGauge(metricName string,mectricValueStr string, ms *MemStorage) int {
+	metricValue, err := strconv.ParseFloat(mectricValueStr,64) 
+	if err == nil {
+		ms.ChangeGaugeValue(metricName,metricValue)
+		return http.StatusOK
+	} else {
+		return http.StatusBadRequest
+	}
+}
+
+func checkDataAndUpdateCounter(metricName string,mectricValueStr string, ms *MemStorage) int {
+	metricValue, err := strconv.ParseInt(mectricValueStr, 10,64) 
+	if err == nil {
+		ms.ChangeCounterValue(metricName,metricValue)
+		return http.StatusOK
+	} else {
+		return http.StatusBadRequest
+	}
+}
+
+func checkDataAndUpdateMetric(metricType string, metricName string,mectricValueStr string, ms *MemStorage) int {
+	if "gauge" == metricType {
+		return checkDataAndUpdateGauge(metricName,mectricValueStr,ms)
+	} else { // if "counter" == metricType
+		return checkDataAndUpdateCounter(metricName,mectricValueStr,ms)
+	}
+}
+
+func updateMetrics(r *http.Request, ms *MemStorage) int {
+	if http.MethodPost == r.Method {
+		newMetricsInfo := strings.Split(r.URL.String(), "/")[2:] 
+		if len(newMetricsInfo) < 3 {
+			return http.StatusNotFound
+			
+		}
+		metricType := strings.ToLower(newMetricsInfo[0])
+		metricName := strings.ToLower(newMetricsInfo[1])
+		mectricValueStr := strings.ToLower(newMetricsInfo[2])
+
+		if "gauge" == metricType || "counter" == metricType {
+			return checkDataAndUpdateMetric(metricType,metricName,mectricValueStr, ms)
+		} else {
+			return http.StatusBadRequest
+		}
+	} else {
+		return http.StatusMethodNotAllowed
+	}
+	
+}
+
+func main() {
+	var ms MemStorage
+	ms.gauge = make(map[string]float64)
+	ms.counter = make(map[string]int64)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		statusCode := updateMetrics(r,&ms)
+		w.WriteHeader(statusCode)
+	})
+
+    err := http.ListenAndServe(`:8080`, mux)
+    if err != nil {
+        panic(err)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YaNeAndrey/ya-metrics
+
+go 1.21.6
