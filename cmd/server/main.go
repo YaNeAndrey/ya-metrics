c655a6372168b31c625af7a03095d0c3d55da2ca
YaNeAndrey <andrey.bery@yandex.ru>
2024-02-09 08:34:58 -0800

    Increment without new tests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,11 +4,45 @@ import (
     "net/http"
 	"github.com/YaNeAndrey/ya-metrics/internal/storage"
 	"github.com/YaNeAndrey/ya-metrics/internal/server/handlers"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	ms := *storage.NewMemStorage()
 
+	ms := storage.NewMemStorage()
+
+	ms.UpdateGaugeMetric("firstGauge", 123)
+	ms.UpdateGaugeMetric("SecondGauge", 1241254235)
+	ms.UpdateCounterMetric("CounterMetric", 1241254235)
+	
+	r := chi.NewRouter()
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+			handlers.HandleGetRoot(rw,r,ms)
+		}) // rootHandle - return all metrics in html table (Change Content type to html)
+
+		r.Route("/value", func(r chi.Router) {
+			r.Get("/{type}/{name}",func(rw http.ResponseWriter, r *http.Request) {
+				handlers.HandleGetMetricValue(rw,r,ms)
+			}) // HandleGetMetricValue - return value for metric {name}
+		})
+
+		r.Route("/update", func(r chi.Router) {
+			r.Post("/{type}/{name}/{value}", func(rw http.ResponseWriter, r *http.Request) {
+				handlers.HandlePostUpdateMetricValue(rw,r,ms)
+			})
+		})
+	}) 
+
+	err:=http.ListenAndServe(":8080", r)
+	if err != nil {
+        panic(err)
+    }
+
+
+/*
 	mux := http.NewServeMux()
 	mux.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
 		if http.MethodPost == r.Method {
@@ -22,4 +56,5 @@ func main() {
     if err != nil {
         panic(err)
     }
+	*/
 }
--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -4,31 +4,122 @@ import (
     "net/http"
 	"strconv"
 	"strings"
+	"html/template"
+	"fmt"
+
+	
+	"github.com/go-chi/chi/v5"
 	"github.com/YaNeAndrey/ya-metrics/internal/storage"
 )
 
-func HandleUpdateMetrics(w http.ResponseWriter, r *http.Request,ms *storage.MemStorage){
-	//Content-Type: text/plain
+
+const tplStr = `<table>
+    <thead>
+        <tr>
+            <th>Metric Name</th>
+            <th>Metric Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        {{range $key, $value := . }}
+			<tr>
+                <td>{{ $key }}</td>
+                <td>{{ $value }}</td>
+            </tr>
+        {{ end }}
+    </tbody>
+</table>`
+
+
+func HandleGetRoot(w http.ResponseWriter, r *http.Request,ms *storage.MemStorage){
 	
-	newMetricsInfo := strings.Split(r.URL.String(), "/")[2:] 
-	if len(newMetricsInfo) < 3 {
+	bufMetricMap := make(map[string]string)
+
+	for key, value := range(ms.ListAllGaugeMetrics()){
+		bufMetricMap[key] = fmt.Sprintf("%v",value)
+	}
+	for key, value := range(ms.ListAllCounterMetrics()){
+		bufMetricMap[key] = fmt.Sprintf("%v",value)
+	}
+
+	tpl, err := template.New("table").Parse(tplStr)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+		return
+    }
+
+    err = tpl.Execute(w, bufMetricMap)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+		return
+    }
+
+	w.Header().Set("Content-Type", "text/html")
+}
+
+func HandleGetMetricValue(w http.ResponseWriter, r *http.Request,ms *storage.MemStorage){
+	metricType := chi.URLParam(r, "type")
+	metricName := chi.URLParam(r, "name")
+
+	body := ""
+	statusCode := http.StatusBadRequest
+
+	switch metricType {
+	case "gauge": 
+		body,statusCode = getGaugeMetricValue(metricName,ms)
+	case "counter":{
+		body,statusCode = getCounterMetricValue(metricName,ms)
+	}
+	default:
+		body,statusCode = "", http.StatusNotFound
+	}
+	_ = statusCode
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(statusCode)
+	w.Write([]byte(body))
+
+	
+}
+
+func HandlePostUpdateMetricValue(w http.ResponseWriter, r *http.Request,ms *storage.MemStorage){
+	metricType := strings.ToLower(chi.URLParam(r, "type"))
+	metricName := chi.URLParam(r, "name")
+	metricValueStr := chi.URLParam(r, "value")
+
+	if metricType == "" || metricName == "" || metricValueStr == "" {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-/*
-	if r.Header.Get("Content-Type") != "text/plain" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-*/
-	metricType := strings.ToLower(newMetricsInfo[0])
-	metricName := newMetricsInfo[1]
-	metricValueStr := newMetricsInfo[2]
-	statusCode := updateMetrics(metricType, metricName,metricValueStr,ms)
+	statusCode := updateMetric(metricType, metricName,metricValueStr,ms)
 	w.WriteHeader(statusCode)
 }
 
-func updateMetrics(metricType string, metricName string,metricValueStr string, ms *storage.MemStorage) int {
+
+func getGaugeMetricValue(metricName string, ms *storage.MemStorage) (string, int) {
+	allGaugeMetrics := ms.ListAllGaugeMetrics()
+
+	value, isExist := allGaugeMetrics[metricName]
+	if isExist {
+		valueStr := strconv.FormatFloat(value, 'f', 0, 64)
+		return valueStr, http.StatusOK
+	} else {
+		return "", http.StatusNotFound
+	}
+}
+
+func getCounterMetricValue(metricName string, ms *storage.MemStorage) (string, int) {
+	allCounterMetrics := ms.ListAllCounterMetrics()
+	value,isExist := allCounterMetrics[metricName]
+	if isExist {
+		valueStr := strconv.FormatInt(value, 10)
+			return valueStr, http.StatusOK
+	} else {
+		return "", http.StatusNotFound
+	}
+}
+
+func updateMetric(metricType string, metricName string,metricValueStr string, ms *storage.MemStorage) int {
 	switch metricType {
 	case "gauge":
 		return checkDataAndUpdateGauge(metricName,metricValueStr,ms)
