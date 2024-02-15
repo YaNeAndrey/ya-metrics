c76483c5269685180a75b40e179e5a8774d4c579
YaNeAndrey <andrey.bery@yandex.ru>
2024-02-15 07:36:56 -0800

    .

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "net/http"
 	"fmt"
+	"net/http"
 
 	"github.com/YaNeAndrey/ya-metrics/internal/server/router"
 	"github.com/YaNeAndrey/ya-metrics/internal/storage"
 )
 
 func main() {
-	conf:= parseFlags()
+	conf := parseFlags()
 	ms := storage.NewMemStorage()
 	r := router.InitRouter(ms)
 
-	err:=http.ListenAndServe(fmt.Sprintf("%s:%d",conf.SrvAddr(),conf.SrvPort()), r)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", conf.SrvAddr(), conf.SrvPort()), r)
 	if err != nil {
-        panic(err)
-    }
+		panic(err)
+	}
 }
